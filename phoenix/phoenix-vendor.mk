c124bb7725ab66acc0f8175d03e6b74caaa19cb6
rupamxx <soumyadeepghosh99@gmail.com>
2020-09-30 00:14:34 +0530

    transform into Lineage tree

--- phoenix/phoenix-vendor.mk
+++ phoenix/phoenix-vendor.mk
@@ -61,6 +61,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/phoenix/proprietary/product/vendor_overlay/29/lib64/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/soundfx/libvolumelistener.so \
     vendor/xiaomi/phoenix/proprietary/product/vendor_overlay/29/lib/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/soundfx/libvolumelistener.so
 
+
 PRODUCT_PACKAGES += \
     libantradio \
     QtiTelephonyService \
